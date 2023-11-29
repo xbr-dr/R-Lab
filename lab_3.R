13b6444703d1c2d18fd130ec10694ee2086b806d
Zubair Dar <49087267+xbr-dr@users.noreply.github.com>
2023-11-29 11:13:49 +0530

    Add files via upload

---
+++ lab_3.R
@@ -0,0 +1,8 @@
+# First line is trick to write multi-line comments
+if (FALSE){ 
+print("Write a R program to create a sequence of numbers from 20 to 50 and find the mean of 
+numbers from 20 to 60 and sum of numbers from 51 to 91.")}
+
+cat("The sequence of numbers from 20 to 50 is:", seq(20,50))
+cat("The mean of numbers from 20 to 60 is:", mean(20:50), "\n")
+cat("The sum of numbers from 51 to 91 :", sum(51:91), "\n")
